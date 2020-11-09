922973cdb8657f42843eb364578e6e6d0e3f64ea
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-09 11:42:15 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {  } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // Importing the top-level component
 import App from './components/App'
