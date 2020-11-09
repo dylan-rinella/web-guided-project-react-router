ac102de51d17edef18a31f98f6e9bb0afa4dfaa2
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-09 11:49:11 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
+import { Route, Link, Switch }
 
 // Components used for the different routes
 import Home from './Home'
